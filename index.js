8ea6c5794ddf7d86e525005031c306109715cacb
leandromoniz <leandromoniz55@gmail.com>
2023-12-16 14:59:39 -0300

    feat: criado class erro e acertado erros

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const conn = require('./db/conn');
 const bcrypt = require('bcrypt');
 const User = require('./models/user');
-const Log = require('./models/log')
 
 const app = express();
 // Config JSON response
@@ -18,25 +17,25 @@ app.use(express.static('public'));
 
 // Função para criar o primeiro administrador se não existir
 const createFirstAdmin = async () => {
-    try {
-        const adminExists = await User.findOne({ where: { isAdmin: true } });
+  try {
+    const adminExists = await User.findOne({ where: { isAdmin: true } });
 
-        if (!adminExists) {
-            const hashedPassword = bcrypt.hashSync(process.env.FIRST_PASSWORD, 10);
-            await User.create({
-                name: process.env.FIRST_NAME,
-                email: process.env.FIRST_EMAIL,
-                password: hashedPassword,
-                isAdmin: true,
-                bit: true,
-            });
-            console.log('Primeiro administrador criado com sucesso');
-        } else {
-            console.log('O administrador já existe');
-        }
-    } catch (error) {
-        console.error('Erro ao criar o primeiro administrador:', error);
+    if (!adminExists) {
+      const hashedPassword = bcrypt.hashSync(process.env.FIRST_PASSWORD, 10);
+      await User.create({
+        name: process.env.FIRST_NAME,
+        email: process.env.FIRST_EMAIL,
+        password: hashedPassword,
+        isAdmin: true,
+        bit: true,
+      });
+      console.log('Primeiro administrador criado com sucesso');
+    } else {
+      console.log('O administrador já existe');
     }
+  } catch (error) {
+    console.error('Erro ao criar o primeiro administrador:', error);
+  }
 };
 
 // Routes
@@ -46,14 +45,14 @@ app.use('/users', UserRoutes);
 
 
 conn
-    .sync()
-    //.sync({ force: true })
-    .then(async () => {
-        // Função para criar o primeiro administrador se não existir
-        await createFirstAdmin();
+  .sync()
+  //.sync({ force: true })
+  .then(async () => {
+    // Função para criar o primeiro administrador se não existir
+    await createFirstAdmin();
 
-        app.listen(5000, () => {
-            console.log('O servidor está rodando na porta 5000');
-        });
-    })
-    .catch((err) => console.log(err));
+    app.listen(5000, () => {
+      console.log('O servidor está rodando na porta 5000');
+    });
+  })
+  .catch((err) => console.log(err));
